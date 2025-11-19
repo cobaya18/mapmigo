68c8c86939f92b463c1652a68c924eb2ed03d0e2
cobaya18 <cobaya18@gmail.com>
2025-11-18 22:11:01 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,7 @@
 self.addEventListener("install", () => {
   console.log("Service worker installed");
 });
+
+self.addEventListener("activate", () => {
+  console.log("Service worker activated");
+});
